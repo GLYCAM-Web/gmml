08d590b99bb27d4426bb917a6a6364ce9a1898d8
alireza <alireza.khatamian.oskooei@gmail.com>
2014-01-06 12:36:38 -0500

    Pdb file reader has been changed

--- includes/FileSet/PdbFileSpace/pdbfile.hpp
+++ includes/FileSet/PdbFileSpace/pdbfile.hpp
@@ -85,6 +85,45 @@ namespace PdbFileSpace
               * @param in_file A stream contains whole contents of a pdb file
               */
             void Read(std::ifstream& in_file);
+            void ParseCards(std::ifstream& in_stream);
+            void ParseHeaderCard(std::ifstream& stream, std::string& line);
+            void ParseObsoleteCard(std::ifstream& stream, std::string& line);
+            void ParseTitleCard(std::ifstream& stream, std::string& line);
+            void ParseSplitCard(std::ifstream& stream, std::string& line);
+            void ParseCaveatCard(std::ifstream& stream, std::string& line);
+            void ParseCompoundCard(std::ifstream& stream, std::string& line);
+            void ParseSourceCard(std::ifstream& stream, std::string& line);
+            void ParseKeywordCard(std::ifstream& stream, std::string& line);
+            void ParseExpirationDateCard(std::ifstream& stream, std::string& line);
+            void ParseNumModelCard(std::ifstream& stream, std::string& line);
+            void ParseModelTypeCard(std::ifstream& stream, std::string& line);
+            void ParseAuthorCard(std::ifstream& stream, std::string& line);
+            void ParseRevisionDateCard(std::ifstream& stream, std::string& line);
+            void ParseSupersededEntriesCard(std::ifstream& stream, std::string& line);
+            void ParseJournalCard(std::ifstream& stream, std::string& line);
+            void ParseRemarkCard(std::ifstream& stream, std::string& line);
+            void ParseDatabaseReferenceCard(std::ifstream& stream, std::string& line);
+            void ParseSequenceAdvancedCard(std::ifstream& stream, std::string& line);
+            void ParseSequenceResidueCard(std::ifstream& stream, std::string& line);
+            void ParseModificationResidueCard(std::ifstream& stream, std::string& line);
+            void ParseHeterogenCard(std::ifstream& stream, std::string& line);
+            void ParseHeterogenNameCard(std::ifstream& stream, std::string& line);
+            void ParseHeterogenSynonymCard(std::ifstream& stream, std::string& line);
+            void ParseFormulaCard(std::ifstream& stream, std::string& line);
+            void ParseHelixCard(std::ifstream& stream, std::string& line);
+            void ParseSheetCard(std::ifstream& stream, std::string& line);
+            void ParseDisulfideBondCard(std::ifstream& stream, std::string& line);
+            void ParseLinkCard(std::ifstream& stream, std::string& line);
+            void ParseCISPeptideCard(std::ifstream& stream, std::string& line);
+            void ParseSiteCard(std::ifstream& stream, std::string& line);
+            void ParseCrystallographyCard(std::ifstream& stream, std::string& line);
+            void ParseOriginCard(std::ifstream& stream, std::string& line);
+            void ParseScaleCard(std::ifstream& stream, std::string& line);
+            void ParseMatrixCard(std::ifstream& stream, std::string& line);
+            void ParseModelCard(std::ifstream& stream, std::string& line);
+            void ParseConnectivityCard(std::ifstream& stream, std::string& line);
+            void ParseMasterCard(std::ifstream& stream, std::string& line);
+            void ParseEndCard(std::ifstream& stream, std::string& line);
 
             //////////////////////////////////////////////////////////
             //                      DISPLAY FUNCTION                //
