e23a926a023227b8e70e3665345f440d90b79316
Delaram <delaramr@uga.edu>
2014-01-08 17:42:49 -0500

    parser for PdbResidueSequence, ResidueModification and PdbHeterogen have been implemented

--- src/FileSet/PdbFileSpace/pdbresiduesequence.cc
+++ src/FileSet/PdbFileSpace/pdbresiduesequence.cc
@@ -2,6 +2,7 @@
 
 #include "../../../includes/FileSet/PdbFileSpace/pdbresiduesequence.hpp"
 #include "../../../includes/common.hpp"
+#include "../../../includes/utils.hpp"
 
 using namespace std;
 using namespace PdbFileSpace;
@@ -20,6 +21,30 @@ PdbResidueSequence::PdbResidueSequence(char chain_id, int number_of_residues, co
     {
         residue_names_.push_back(*it);
     }
+}
+
+PdbResidueSequence::PdbResidueSequence(stringstream& stream_block)
+{
+    string line;
+    bool is_chain_id_set = false, is_number_of_residues_set = false;
+    stringstream ss;
+    getline(stream_block, line);
+    line = Trim(line);
+    while (!Trim(line).empty())
+    {
+        if(!is_chain_id_set){
+            chain_id_ = ConvertString<char>(line.substr(11,1));
+            is_chain_id_set=true;
+        }
+        if(!is_number_of_residues_set){
+            number_of_residues_ = ConvertString<int>(line.substr(13,4));
+            is_number_of_residues_set=true;
+        }
+        ss << line.substr(19,51) << " ";
+
+        getline(stream_block, line);
+    }
+    residue_names_ = Split(ss.str(), " ");
 }
 
 //////////////////////////////////////////////////////////
