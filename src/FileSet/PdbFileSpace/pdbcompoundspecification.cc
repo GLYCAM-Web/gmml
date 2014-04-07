28c14d85f350e183e8edfe3e664f3e078a5640d4
alireza <alireza.khatamian.oskooei@gmail.com>
2014-04-07 11:27:33 -0400

    PdbCompoundSpecification and PdbConnectCard issues have been fixed

--- src/FileSet/PdbFileSpace/pdbcompoundspecification.cc
+++ src/FileSet/PdbFileSpace/pdbcompoundspecification.cc
@@ -26,10 +26,14 @@ PdbCompoundSpecification::PdbCompoundSpecification(stringstream& specification_b
     string line;
     getline(specification_block, line);
     string temp = line;
+    int flag = 0;
+    stringstream molecule_name, chain_id, fragment, molecule_synonyms, enzyme_commission_numbers, comments;
     while(!Trim(temp).empty())
     {
-        vector<string> tokens = Split(line,":");
+        vector<string> tokens = Split(line,":;");
+
         string token_name = Trim(tokens.at(0));
+
         if(token_name == "MOL_ID")
         {
             molecule_id_ = tokens.at(1);
@@ -37,37 +41,33 @@ PdbCompoundSpecification::PdbCompoundSpecification(stringstream& specification_b
         }
         else if(token_name == "MOLECULE")
         {
-            molecule_name_ = tokens.at(1);
-            Trim(molecule_name_);
+            string s = tokens.at(1);
+            molecule_name << s;
+            flag = 1;
         }
         else if(token_name == "CHAIN")
         {
-            chain_ids_ = Split(tokens.at(1),",;");
-            for(vector<string>::iterator it = chain_ids_.begin(); it != chain_ids_.end(); it++)
-            {
-                Trim(*it);
-            }
+            string s = tokens.at(1);
+            chain_id << s;
+            flag = 2;
         }
         else if(token_name == "FRAGMENT")
         {
-            fragment_ = tokens.at(1);
-            Trim(fragment_);
+            string s = tokens.at(1);
+            fragment << s;
+            flag = 3;
         }
         else if(token_name == "SYNONYM")
         {
-            molecule_synonyms_ = Split(tokens.at(1), ",;");
-            for(vector<string>::iterator it = molecule_synonyms_.begin(); it != molecule_synonyms_.end(); it++)
-            {
-                Trim(*it);
-            }
+            string s = tokens.at(1);
+            molecule_synonyms << s;
+            flag = 4;
         }
         else if(token_name == "EC")
         {
-            enzyme_commission_numbers_ = Split(tokens.at(1), ",;");
-            for(vector<string>::iterator it = enzyme_commission_numbers_.begin(); it != enzyme_commission_numbers_.end(); it++)
-            {
-                Trim(*it);
-            }
+            string s = tokens.at(1);
+            enzyme_commission_numbers << s;
+            flag = 5;
         }
         else if(token_name == "ENGINEERED")
         {
@@ -87,14 +87,88 @@ PdbCompoundSpecification::PdbCompoundSpecification(stringstream& specification_b
         }
         else if(token_name == "OTHER_DETAILS")
         {
-            comments_ = tokens.at(1);
-            Trim(comments_);
+            string s = tokens.at(1);
+            comments << s;
+            flag = 6;
         }
         else
         {
+            switch (flag)
+            {
+                case 1:
+                    molecule_name << line;
+                    break;
+                case 2:
+                    chain_id << line;
+                    break;
+                case 3:
+                    fragment << line;
+                    break;
+                case 4:
+                    molecule_synonyms << line;
+                    break;
+                case 5:
+                    enzyme_commission_numbers << line;
+                    break;
+                case 6:
+                    comments << line;
+                    break;
+            }
         }
         getline(specification_block, line);
         temp = line;
+    }
+    if(molecule_name.str().length() > 0)
+    {
+        molecule_name_ = Split(molecule_name.str(),";").at(0);
+        Trim(molecule_name_);
+    }
+
+    if(chain_id.str().length() > 0)
+    {
+        string chain_ids = chain_id.str();
+        Trim(chain_ids);
+        chain_ids_ = Split(chain_ids,",;");
+//        for(vector<string>::iterator it = chain_ids_.begin(); it != chain_ids_.end(); it++)
+//        {
+//            Trim(*it);
+//        }
+    }
+
+    if(fragment.str().length() > 0)
+    {
+        fragment_ = fragment.str();
+        Trim(fragment_);
+        fragment_ = Split(fragment_, ";").at(0);
+        Trim(fragment_);
+    }
+
+    if(molecule_synonyms.str().length() > 0)
+    {
+        string synonyms = molecule_synonyms.str();
+        Trim(synonyms);
+        molecule_synonyms_ = Split(synonyms, ",;");
+//        for(vector<string>::iterator it = molecule_synonyms_.begin(); it != molecule_synonyms_.end(); it++)
+//        {
+//            Trim(*it);
+//        }
+    }
+
+    if(enzyme_commission_numbers.str().length() > 0)
+    {
+        string commission_numbers = enzyme_commission_numbers.str();
+        Trim(commission_numbers);
+        enzyme_commission_numbers_ = Split(commission_numbers, ",;");
+//        for(vector<string>::iterator it = enzyme_commission_numbers_.begin(); it != enzyme_commission_numbers_.end(); it++)
+//        {
+//            Trim(*it);
+//        }
+    }
+
+    if(comments.str().length() > 0)
+    {
+        comments_ = Split(comments.str(),";").at(0);
+        Trim(comments_);
     }
 }
 
