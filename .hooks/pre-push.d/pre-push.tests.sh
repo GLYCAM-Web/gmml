e5a2f46a02df5fc056ccbadc9c874827e94f8e02
oliver <olivercgrant@gmail.com>
2017-06-23 17:30:24 +0200

    testing pre-push tests

---
+++ .hooks/pre-push.d/pre-push.tests.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+check_gemshome() {
+   if [ -z "$GEMSHOME" ]; then
+      echo ""
+      echo "Your GEMSHOME environment variable is not set! It should be set to"
+      echo "$1"
+      exit 1
+   elif [ ! -d $GEMSHOME ]; then
+      echo ""
+      echo "Your GEMSHOME environment variable is set to $GEMSHOME -- this does"
+      echo "not appear to be a directory. It should be set to"
+      echo "$1"
+      exit 1
+   elif [ ! "$GEMSHOME" = "$1" -a ! "$GEMSHOME" = "${1}/" ]; then
+      #try checking the inode incase there is a problem with symlinks
+       if [ `stat -c "%i" $GEMSHOME` != `stat -c "%i" ${1}` ]; then
+           echo ""
+           echo "ERROR: GEMSHOME is expected to be $1 but it is currently"
+           echo "$GEMSHOME    This will cause problems!"
+           exit 1
+       fi
+   fi
+}
+
+cd ../
+gemshome=`pwd`
+cd -
+check_gemshome $gemshome 
+
+echo "Running mandatory tests..."
+cd $GEMSHOME/gmml/tests/
+ bash compile_run_tests.bash
+cd -
+
+if [ -f $GEMSHOME/gmml/tests/All_Tests_Passed ] ; then
+    echo  "All tests have passed. Commits are now allowed."
+    exit 0
+else
+    echo "
+         *****************************************************************
+         You're a naughty boy, Fawlty!
+         Tests have not been run, or have failed! 
+         Push cancelled.
+         *****************************************************************
+         "
+    exit 1
+fi
