7b84f0e2d9850c32b19d4b20cf7efe49743b6565
David Montgomery <dwm19550@uga.edu>
2018-06-04 14:20:55 -0400

    Added notes

--- src/MolecularModeling/Assembly/Ontology/gf_query.cc
+++ src/MolecularModeling/Assembly/Ontology/gf_query.cc
@@ -3,51 +3,101 @@
 std::string MolecularModeling::Assembly::QueryOntology(std::string searchType, std::string searchTerm, std::string url, std::string output_file_type)
 {
     std::stringstream query;
-    query << Ontology::PREFIX << Ontology::SELECT_CLAUSE;
-    query << "?pdb ?oligo_sequence ?residue_links ?glycosidic_linkage ?title"
-             "?resolution ?Mean_B_Factor ?oligo_mean_B_Factor ?authors ?journal ?PMID ?DOI \n"; 
-    query << Ontology::WHERE_CLAUSE;
-    query << "?oligo        :oligoName              ?oligo_sequence.\n";
-    query << "?pdb_file     :identifier             ?pdb.\n";
-    query << "?pdb_file     :hasTitle               ?title.\n";
-    query << "OPTIONAL {";
-    query << "?pdb_file     :hasResolution          ?resolution.}\n";
-    query << "?pdb_file     :hasAuthors             ?authors.\n";
-    query << "OPTIONAL {";
-    query << "?pdb_file     :hasJournal             ?journal.}\n";
-    query << "OPTIONAL {";
-    query << "?pdb_file     :hasDOI                 ?DOI.}\n";
-    query << "OPTIONAL {";
-    query << "?pdb_file     :hasPMID                ?PMID.}\n";
-    query << "OPTIONAL {";
-    query << "?pdb_file     :hasBFactor             ?Mean_B_Factor.}\n";
-    query << "OPTIONAL {";
-    query << "?oligo        :oligoName              ?oligo_sequence.}\n";
-    query << "OPTIONAL {";
-    query << "?oligo        :oligoBFactor           ?oligo_mean_B_Factor.}\n";
-    query << "OPTIONAL {";
-    query << "?pdb_file     :hasOligo	              ?oligo.}\n";
-    query << "OPTIONAL {";
-    query << "?oligo        :oligoResidueLinks      ?residue_links.}\n";
-    query << "OPTIONAL {";
-    query << "?linkage      :hasParent 	            ?oligo.}\n";
-    query << "OPTIONAL {";
-    query << "?linkage      :glycosidicLinkage      ?glycosidic_linkage.}\n";
-    query << Ontology::END_WHERE_CLAUSE;
     std::stringstream search;
     search << searchType;
-    if(search.str()=="PDB")
-    {
-      query<<"VALUES ?pdb { \"" << searchTerm << "\" }\n";
+    if(search.str()=="Oligo_REGEX")
+    {      
+      query << Ontology::PREFIX << Ontology::SELECT_CLAUSE;
+      query << "?pdb ?oligo_sequence ?residue_links ?glycosidic_linkage ?title"
+               "?resolution ?Mean_B_Factor ?oligo_mean_B_Factor ?authors ?journal ?PMID ?DOI"
+               "?note_type ?note_category ?description \n"; 
+      query << Ontology::WHERE_CLAUSE;
+      query << "?oligo        :oligoName              ?oligo_sequence.\n";
+      
+      query << "FILTER regex(?oligo_sequence, \"" << searchTerm << "\")";    
+      query << "?pdb_file     :identifier             ?pdb.\n";
+      query << "?pdb_file     :hasTitle               ?title.\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasResolution          ?resolution.}\n";
+      query << "?pdb_file     :hasAuthors             ?authors.\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasJournal             ?journal.}\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasDOI                 ?DOI.}\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasPMID                ?PMID.}\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasBFactor             ?Mean_B_Factor.}\n";
+      query << "OPTIONAL {";
+      query << "?oligo        :oligoName              ?oligo_sequence.}\n";
+      query << "OPTIONAL {";
+      query << "?oligo        :oligoBFactor           ?oligo_mean_B_Factor.}\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasOligo	              ?oligo.}\n";
+      query << "OPTIONAL {";
+      query << "?oligo        :oligoResidueLinks      ?residue_links.}\n";
+      query << "OPTIONAL {";
+      query << "?linkage      :hasParent 	            ?oligo.}\n";
+      query << "OPTIONAL {";
+      query << "?linkage      :glycosidicLinkage      ?glycosidic_linkage.}\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file      :hasNote    ?note.}\n";
+      query << "OPTIONAL {";
+      query << "?note	       :NoteType    ?note_type.}\n";
+      query << "OPTIONAL {";
+      query << "?note	       :NoteCategory    ?note_category.}\n";
+      query << "OPTIONAL {";
+      query << "?note	       :description    ?description.}\n";
+      query << Ontology::END_WHERE_CLAUSE;
     }
-    else if(search.str()=="Condensed_Sequence")
+    else
     {
-      query<<"VALUES ?oligo_sequence { \"" << searchTerm << "\" }\n";
+      query << Ontology::PREFIX << Ontology::SELECT_CLAUSE;
+      query << "?pdb ?oligo_sequence ?residue_links ?glycosidic_linkage ?title"
+               "?resolution ?Mean_B_Factor ?oligo_mean_B_Factor ?authors ?journal ?PMID ?DOI \n"; 
+      query << Ontology::WHERE_CLAUSE;
+      query << "?oligo        :oligoName              ?oligo_sequence.\n";
+      query << "?pdb_file     :identifier             ?pdb.\n";
+      query << "?pdb_file     :hasTitle               ?title.\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasResolution          ?resolution.}\n";
+      query << "?pdb_file     :hasAuthors             ?authors.\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasJournal             ?journal.}\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasDOI                 ?DOI.}\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasPMID                ?PMID.}\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasBFactor             ?Mean_B_Factor.}\n";
+      query << "OPTIONAL {";
+      query << "?oligo        :oligoName              ?oligo_sequence.}\n";
+      query << "OPTIONAL {";
+      query << "?oligo        :oligoBFactor           ?oligo_mean_B_Factor.}\n";
+      query << "OPTIONAL {";
+      query << "?pdb_file     :hasOligo	              ?oligo.}\n";
+      query << "OPTIONAL {";
+      query << "?oligo        :oligoResidueLinks      ?residue_links.}\n";
+      query << "OPTIONAL {";
+      query << "?linkage      :hasParent 	            ?oligo.}\n";
+      query << "OPTIONAL {";
+      query << "?linkage      :glycosidicLinkage      ?glycosidic_linkage.}\n";
+      query << Ontology::END_WHERE_CLAUSE;
+      
+      if(search.str()=="PDB")
+      {
+        query<<"VALUES ?pdb { \"" << searchTerm << "\" }\n";
+      }
+      else if(search.str()=="Condensed_Sequence")
+      {
+        gmml::FindReplaceString(searchTerm, "[", "\\\\[");
+        gmml::FindReplaceString(searchTerm, "]", "\\\\]");
+        gmml::FindReplaceString(searchTerm, "-OH", "-ROH");
+        query<<"VALUES ?oligo_sequence { \"" << searchTerm << "\" }\n";
+      }
     }
-    else if(search.str()=="Oligo_Library")
-    {
-      // query<<"VALUES ?pdb { \"" << searchTerm << "\" }\n";
-    }
+    
+    
     
 
     return FormulateCURLGF(output_file_type, query.str(), url);
