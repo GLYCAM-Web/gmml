443eef68f0a67e734b44b77f1a18afee3fcb5ac6
Lachele Foley <lachele@gmail.com>
2018-06-27 17:57:43 -0400

    updating metadata with gauche effect info

--- includes/MolecularMetadata/GLYCAM/glycam06residueinfo.hpp
+++ includes/MolecularMetadata/GLYCAM/glycam06residueinfo.hpp
@@ -5,7 +5,7 @@
  * GLYCAM06 metadata for residues
  *
  * This file is enerated automatically on:
- *     Fri Jun 22 16:48:59 EDT 2018
+ *     Wed Jun 27 17:56:06 EDT 2018
  *
  * by a script named:
  *     GLYCAM06_make_glycam06_residue_name-2-type_lookup.bash
