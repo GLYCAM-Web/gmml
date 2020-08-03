a7f528bc5a95e76968d032e1cd33a10701b8e603
gitoliver <olivercgrant@gmail.com>
2020-08-03 13:46:06 +0200

    Added initialize function to PdbFile to reduce repetative code

--- src/InputSet/PdbFileSpace/pdbfile.cc
+++ src/InputSet/PdbFileSpace/pdbfile.cc
@@ -94,90 +94,14 @@ using PdbFileSpace::PdbFile;
 //////////////////////////////////////////////////////////
 PdbFile::PdbFile()
 {
+    this->Initialize();
     path_ = "GMML-Generated";
-    header_ = NULL;
-    obsolete_ = NULL;
-    title_ = NULL;
-    split_ = NULL;
-    caveat_ = NULL;
-    compound_ = NULL;
-    source_ = NULL;
-    keywords_ = NULL;
-    experimental_data_ = NULL;
-    number_of_models_ = NULL;
-    model_type_ = NULL;
-    author_ = NULL;
-    revision_data_ = NULL;
-    superseded_entries_ = NULL;
-    journal_ = NULL;
-    remark_cards_ = NULL;
-    database_reference_ = NULL;
-    sequence_advanced_ = NULL;
-    residues_sequence_ = NULL;
-    residue_modification_cards_ = NULL;
-    heterogen_cards_ = NULL;
-    heterogen_name_cards_ = NULL;
-    heterogen_synonym_cards_ = NULL;
-    formulas_ = NULL;
-    helix_cards_ = NULL;
-    sheet_cards_ = NULL;
-    disulfide_bonds_ = NULL;
-    link_cards_ = NULL;
-    cis_peptide_ = NULL;
-    site_cards_ = NULL;
-    crystallography_ = NULL;
-    origins_ = NULL;
-    scales_ = NULL;
-    matrices_ = NULL;
-    models_ = NULL;
-    connectivities_ = NULL;
-    serial_number_mapping_ = PdbFile::PdbSerialNumberMapping();
-    sequence_number_mapping_ = PdbFile::PdbSequenceNumberMapping();
-    master_ = NULL;
 }
 
 PdbFile::PdbFile(const std::string &pdb_file)
 {
+    this->Initialize();
     path_ = pdb_file;
-    header_ = NULL;
-    obsolete_ = NULL;
-    title_ = NULL;
-    split_ = NULL;
-    caveat_ = NULL;
-    compound_ = NULL;
-    source_ = NULL;
-    keywords_ = NULL;
-    experimental_data_ = NULL;
-    number_of_models_ = NULL;
-    model_type_ = NULL;
-    author_ = NULL;
-    revision_data_ = NULL;
-    superseded_entries_ = NULL;
-    journal_ = NULL;
-    remark_cards_ = NULL;
-    database_reference_ = NULL;
-    sequence_advanced_ = NULL;
-    residues_sequence_ = NULL;
-    residue_modification_cards_ = NULL;
-    heterogen_cards_ = NULL;
-    heterogen_name_cards_ = NULL;
-    heterogen_synonym_cards_ = NULL;
-    formulas_ = NULL;
-    helix_cards_ = NULL;
-    sheet_cards_ = NULL;
-    disulfide_bonds_ = NULL;
-    link_cards_ = NULL;
-    cis_peptide_ = NULL;
-    site_cards_ = NULL;
-    crystallography_ = NULL;
-    origins_ = NULL;
-    scales_ = NULL;
-    matrices_ = NULL;
-    models_ = NULL;
-    connectivities_ = NULL;
-    serial_number_mapping_ = PdbFile::PdbSerialNumberMapping();
-    sequence_number_mapping_ = PdbFile::PdbSequenceNumberMapping();
-    master_ = NULL;
     std::ifstream in_file;
     if(std::ifstream(pdb_file.c_str()))
     {
@@ -240,46 +164,7 @@ PdbFile::PdbFile(const std::string &pdb_file)
 
 PdbFile::PdbFile(std::stringstream& atomStream)
 {
-  path_ = "";
-  header_ = NULL;
-  obsolete_ = NULL;
-  title_ = NULL;
-  split_ = NULL;
-  caveat_ = NULL;
-  compound_ = NULL;
-  source_ = NULL;
-  keywords_ = NULL;
-  experimental_data_ = NULL;
-  number_of_models_ = NULL;
-  model_type_ = NULL;
-  author_ = NULL;
-  revision_data_ = NULL;
-  superseded_entries_ = NULL;
-  journal_ = NULL;
-  remark_cards_ = NULL;
-  database_reference_ = NULL;
-  sequence_advanced_ = NULL;
-  residues_sequence_ = NULL;
-  residue_modification_cards_ = NULL;
-  heterogen_cards_ = NULL;
-  heterogen_name_cards_ = NULL;
-  heterogen_synonym_cards_ = NULL;
-  formulas_ = NULL;
-  helix_cards_ = NULL;
-  sheet_cards_ = NULL;
-  disulfide_bonds_ = NULL;
-  link_cards_ = NULL;
-  cis_peptide_ = NULL;
-  site_cards_ = NULL;
-  crystallography_ = NULL;
-  origins_ = NULL;
-  scales_ = NULL;
-  matrices_ = NULL;
-  models_ = NULL;
-  connectivities_ = NULL;
-  serial_number_mapping_ = PdbFile::PdbSerialNumberMapping();
-  sequence_number_mapping_ = PdbFile::PdbSequenceNumberMapping();
-  master_ = NULL;
+  this->Initialize();
 
   if(!Read(atomStream))
   {
@@ -8278,4 +8163,52 @@ void PdbFile::Print(std::ostream &out)
         out << "******************************** MASTER *******************************" << std::endl;
         master_->Print(out);
     }
+}
+
+/////////////////////////////////////////////////////////
+//                   PRIVATE FUNCTIONS                  //
+//////////////////////////////////////////////////////////
+
+void PdbFile::Initialize()
+{
+  path_ = "";
+  header_ = NULL;
+  obsolete_ = NULL;
+  title_ = NULL;
+  split_ = NULL;
+  caveat_ = NULL;
+  compound_ = NULL;
+  source_ = NULL;
+  keywords_ = NULL;
+  experimental_data_ = NULL;
+  number_of_models_ = NULL;
+  model_type_ = NULL;
+  author_ = NULL;
+  revision_data_ = NULL;
+  superseded_entries_ = NULL;
+  journal_ = NULL;
+  remark_cards_ = NULL;
+  database_reference_ = NULL;
+  sequence_advanced_ = NULL;
+  residues_sequence_ = NULL;
+  residue_modification_cards_ = NULL;
+  heterogen_cards_ = NULL;
+  heterogen_name_cards_ = NULL;
+  heterogen_synonym_cards_ = NULL;
+  formulas_ = NULL;
+  helix_cards_ = NULL;
+  sheet_cards_ = NULL;
+  disulfide_bonds_ = NULL;
+  link_cards_ = NULL;
+  cis_peptide_ = NULL;
+  site_cards_ = NULL;
+  crystallography_ = NULL;
+  origins_ = NULL;
+  scales_ = NULL;
+  matrices_ = NULL;
+  models_ = NULL;
+  connectivities_ = NULL;
+  serial_number_mapping_ = PdbFile::PdbSerialNumberMapping();
+  sequence_number_mapping_ = PdbFile::PdbSequenceNumberMapping();
+  master_ = NULL;
 }
