e4d4be23b6e6cb0519c8cd42c4b42edfc33c6d77
gitoliver <olivercgrant@gmail.com>
2020-09-11 13:12:40 +0200

    Added a print statement to test the carbohydrateBuilder

--- tests/tests/010.buildBySequenceRotamer.cc
+++ tests/tests/010.buildBySequenceRotamer.cc
@@ -14,6 +14,7 @@ int main()
     std::string prep = "../dat/prep/GLYCAM_06j-1.prep";
     std::string condensed_sequence = "DManp[2S,3Me]a1-6DManpa1-6[DGlcpNAcb1-4][DNeu5Aca2-6DGalpb1-4DGlcpNAc[3S]b1-2DManpa1-3]DManpb1-4DGlcpNAc[6Me]b1-4DGlcpNAcb1-OH";
     CondensedSequenceSpace::carbohydrateBuilder carbBuilder(condensed_sequence, prep);
+    carbBuilder.Print();
     //std::cout << carbBuilder.GenerateUserOptionsJSON() << std::endl;
     CondensedSequenceSpace::singleRotamerInfoVector rotamerInfoVector =
 	{ 
