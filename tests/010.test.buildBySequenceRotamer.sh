e7893c653c2a841f90b12d76a67a3a5a7688ddc6
gitoliver <olivercgrant@gmail.com>
2020-09-11 16:05:09 +0200

    Updated tests to reflect the fixes in building specific rotamers

--- tests/010.test.buildBySequenceRotamer.sh
+++ tests/010.test.buildBySequenceRotamer.sh
@@ -2,7 +2,7 @@
 
 printf "Testing buildBySequenceRotamer... "
 g++ -std=c++0x -I $GEMSHOME/gmml/includes/ -L$GEMSHOME/gmml/bin/ -Wl,-rpath,$GEMSHOME/gmml/bin/ tests/010.buildBySequenceRotamer.cc -lgmml -o buildBySequenceRotamer
-./buildBySequenceRotamer > /dev/null 2>&1
+./buildBySequenceRotamer > 010.output_buildBySequenceRotamer.txt
 if [ -f structure.pdb ] && [ -f structure.off ]; then
     if ! cmp structure.pdb tests/correct_outputs/010.buildBySequenceRotamer.pdb > /dev/null 2>&1; then
         printf "Test FAILED!. PDB file different\n"
@@ -10,9 +10,12 @@ if [ -f structure.pdb ] && [ -f structure.off ]; then
     #elif ! cmp structure.off tests/correct_outputs/010.buildBySequenceRotamer.off > /dev/null 2>&1; then
     #    printf "Test FAILED!. Off file different.\n"
     #    return 1;
+    elif ! cmp  010.output_buildBySequenceRotamer.txt tests/correct_outputs/010.output_buildBySequenceRotamer.txt > /dev/null 2>&1; then
+        printf "Test FAILED!. Output file different\n"
+        return 1;
     else
         printf "Test passed.\n"
-        rm structure.pdb structure.off buildBySequenceRotamer
+        rm structure.pdb structure.off buildBySequenceRotamer 010.output_buildBySequenceRotamer.txt
         return 0;
     fi
 else
