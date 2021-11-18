1221e515322bfa4b140d845e0103abc93f19aba0
Oliver <olgrant@uga.edu>
2021-11-18 16:39:00 +0100

    Changed the table thing into a class that I can wrap up into gems.

--- tests/tests/018.test.createGlycosylationTables.cpp
+++ tests/tests/018.test.createGlycosylationTables.cpp
@@ -2,31 +2,7 @@
 #include <string>
 #include <vector>
 #include "includes/MolecularModeling/assembly.hpp"
-#include "includes/MolecularModeling/residue.hpp"
-#include "includes/MolecularMetadata/glycoprotein.hpp"
-
-// Structs
-struct GlycosylationSiteInfo
-{
-	// Constructor
-	GlycosylationSiteInfo(std::string chain, std::string residueNumber, std::string insertionCode, std::string sequenceContext, std::vector<std::string> tags) : chain_(chain), residueNumber_(residueNumber), insertionCode_(insertionCode), sequenceContext_(sequenceContext), tags_(tags) {}
-	// Data
-	std::string chain_;
-	std::string residueNumber_;
-	std::string insertionCode_;
-	std::string sequenceContext_;
-	std::vector<std::string> tags_; // e.g. oLink, nLink, sequon, cysteineSequon, all, etc
-	// Print
-	inline std::string Print()
-	{
-	    std::string output = "Chain: " + chain_ + "\nResidueNumber: " + residueNumber_ + "\nInsertionCode: " + insertionCode_ + "\nSequenceContext: " + sequenceContext_ + "\nTags:\n";
-	    for (auto &tag : tags_ )
-	    {
-	        output += tag + "\n";
-	    }
-	    return output;
-	}
-};
+#include "includes/InternalPrograms/glycosylationSiteFinder.hpp"
 
 // To become the test
 int main(int argc, char* argv[])
@@ -37,24 +13,15 @@ int main(int argc, char* argv[])
         std::cout << "Example: pdb2glycam 1RVX.pdb \n";
         std::exit(EXIT_FAILURE);
     }
-    std::vector<GlycosylationSiteInfo> tableInfo;
-    MolecularModeling::Assembly ass (argv[1], gmml::InputFileType::PDB);
-    ass.BuildStructureByDistance(10); // number of threads to use.
-    ass.GenerateResidueNodesInAssembly();
-    for (auto &residue : ass.GetResidues())
-    {
-    	std::string linkType = glycoproteinMetadata::LookupLinkTypeForAminoAcidName(residue->GetName());
-    	if (!linkType.empty())
-    	{
-        	//std::cout << "Checking: " << residue->GetId() << "(" << residue->GetIndex() << ") with linkType: " << linkType << std::endl;
-    		std::vector<std::string> tags = {linkType};
-        	std::string context = glycoproteinMetadata::GetSequenceContextAndDetermineTags(residue, tags);
-        	tableInfo.emplace_back(residue->GetChainID(), residue->GetNumber(), residue->GetInsertionCode(), context, tags );
-    	}
-    }
-    for (auto &tableElement : tableInfo)
-    {
-        std::cout << tableElement.Print() << "\n";
-    }
+    MolecularModeling::Assembly assembly (argv[1], gmml::InputFileType::PDB);
+    assembly.BuildStructureByDistance(10); // number of threads to use.
+    assembly.GenerateResidueNodesInAssembly();
+    glycoproteinBuilder::GlycosylationSiteFinder siteFinder(assembly);
+    std::cout << siteFinder.PrintTable();
+    std::vector<GlycosylationSiteInfo> tableInfo = siteFinder.GetTable();
+//    for (auto &tableElement : tableInfo)
+//    {
+//        std::cout << tableElement.Print() << "\n";
+//    }
     return 0;
 }
