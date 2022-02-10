2b20c1c6ac7e5a5ff3c0b85e44b63cbf12282bf7
Oliver <olgrant@uga.edu>
2022-02-10 18:55:58 +0100

    Test 019 in place for pdbPreprocessor.

--- tests/019.test.newPDBClass.sh
+++ tests/019.test.newPDBClass.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 printf "Testing 019.test.newPDBClass.cpp... "
 g++ -std=c++17 -I $GEMSHOME/gmml/ -L$GEMSHOME/gmml/bin/ -Wl,-rpath,$GEMSHOME/gmml/bin/ tests/019.test.newPDBClass.cpp -lgmml -pthread -o newPdbClass
-./newPdbClass tests/inputs/4mbz.pdb
-
+for filepath in `ls tests/inputs/019.*.pdb`
+do
+	file=`basename $filepath`
+	./newPdbClass tests/inputs/$file > output.txt
+	if !cmp  output.txt tests/correct_outputs/$file-output.txt > /dev/null 2>&1; then
+    	printf "Test FAILED!. output.txt different from tests/correct_outputs/$file-output.txt\n Compare using diff\n"
+    	return 1;
+	elif !cmp  outputPdbfile.pdb tests/correct_outputs/$file-output.pdb > /dev/null 2>&1; then
+		printf "Test FAILED!. outputPdbfile.pdb different from tests/correct_outputs/$file-output.pdb\n Compare using diff or VMD\n"    	
+	fi
+done
+printf "Test passed.\n"
+rm ./newPdbClass outputPdbFile.pdb output.txt
+return 0
