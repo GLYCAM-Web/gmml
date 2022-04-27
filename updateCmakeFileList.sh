9129205355a92d084ec80c8487e6fcfe7502b2ee
Preston <prestonj@uga.edu>
2022-04-27 04:41:10 -0400

    some cmake cleaning up
    made it so script that grabs all our header and source files is clean. Also added a sep blurb for the external libraries so they will be flagged with -isystem in our compile commands.

--- updateCmakeFileList.sh
+++ updateCmakeFileList.sh
@@ -1,21 +1,92 @@
 #!/bin/bash
 
-#okay this is kinda lazy but if anything goes bad with these commands it will b
-#thrown in the given files
+#NOTE: the -t flag is used to add our test files to the cmake file lists, this is important for linting.
+INCLUDE_TEST_FILES=0
+while getopts "t" option
+do
+        case "${option}" in
+            t)
+                INCLUDE_TEST_FILES=1
+                ;;
+			*)
+                exit 1 
+                ;;
+        esac
+done
 
-if [ -f "./cmakeFileLists/cFileList.txt" ]; then
-        printf "\nPrevious data found, Removing old cc/cpp file list..."
+FILES_UPDATED=0
+
+echo "This script will walk through all the source and header files and put them into a file."
+echo "This was done because it prevents us from having to adhere to some more annoying cmake"
+echo "paradigms."
+echo ""
+
+#Grab all c++ files and chuck them into our file. We have the option to be able to also grab
+#the tests c++ files in case we want to lint/run tests on em
+echo "Hitting the list that contains all the paths to our source files"
+if  [ "${INCLUDE_TEST_FILES}" == 1 ] || ! diff cmakeFileLists/cFileList.txt <(find "./src" -name "*.cc" -o -name "*.cpp" | sort) ; then
+	if [ "${INCLUDE_TEST_FILES}" == 1 ]; then
+        diff cmakeFileLists/cFileList.txt <(find "./src" "./tests" -name "*.cc" -o -name "*.cpp" | sort)
+    fi
+	if [ -f "./cmakeFileLists/cFileList.txt" ]; then
+        echo "Previous data found, Removing old cc/cpp file list..."
         rm ./cmakeFileLists/cFileList.txt
+    fi
+    echo "Grabbing all *.cpp/*.cc files from /src/ and throwing into ./cmakeFileLists/cFileList.txt"
+    if [ "${INCLUDE_TEST_FILES}" == 1 ]; then
+        find "./src" "./tests" -name "*.cc" -o -name "*.cpp" | sort &> ./cmakeFileLists/cFileList.txt
+    else
+        find "./src" -name "*.cc" -o -name "*.cpp" | sort &> ./cmakeFileLists/cFileList.txt
+    fi
+    FILES_UPDATED=1
+    echo "Finished fixing up the list that has all our source files"
+else
+    echo "No changes in our c file list."
 fi
-printf "\nGrabbing all *.cpp/*.cc files from /src/ and throwing into ./cmakeFileLists/cFileList.txt\n"
-find ./src/ -name \*.cc -o -name \*.cpp &> ./cmakeFileLists/cFileList.txt
-#lmk if we want to do a diff type thying to show what we are adding
+echo""
 
-if [ -f "./cmakeFileLists/hDirectoryList.txt" ]; then
-	printf "\nPrevious data found, Removing old header file directory path list..."
-	rm ./cmakeFileLists/hDirectoryList.txt
+#Get all OUR header files, none of the external libraries. This is important because when cmake uses these file lists
+#it will have all these files use the normal -I flag for gcc in the compile commands
+echo "Hitting the list that contains all our paths to our directories that have our headers"
+if ! diff cmakeFileLists/hDirectoryList.txt <( find "./includes" -type d ! -path "*External_Libraries*" | sort ) ; then
+    if [ -f "./cmakeFileLists/hDirectoryList.txt" ]; then
+        echo "Previous data found, Removing old header file directory path list..."
+        rm ./cmakeFileLists/hDirectoryList.txt
+    fi
+    echo "Grabbing all dir paths to our h/hpp files and throwing into ./cmakeFileLists/hDirectoryList.txt"
+    #Grabs all our header code dirs
+    find "./includes" -type d ! -path "*External_Libraries*" | sort   &> ./cmakeFileLists/hDirectoryList.txt
+    FILES_UPDATED=1
+    echo "Finished fixing up the list that has all our include dirs"
+else
+    echo "No changes in our header directory list."
 fi
-printf "\nGrabbing all dir paths to our h/hpp files and throwing into ./cmakeFileLists/hDirectoryList.txt\n"
-find ./includes/ -type d &> ./cmakeFileLists/hDirectoryList.txt
+echo ""
 
-printf "\nFiles updated\n"
+#Gets all our EXTERNAL lib headers. This is Eigen3 and pcg files. This is important because when cmake uses this file
+#list to have all the files use the -isystem flag on the files which prevents linting on em and should help clean up
+#our build outputs.
+echo "Hitting the list that contains all our paths to the directories that have all our 3rd party libraries"
+if ! diff cmakeFileLists/externalHDirectoryList.txt <(find "./includes" -type d -path "*External_Libraries*" | sort) ; then
+    if [ -f "./cmakeFileLists/externalHDirectoryList.txt" ]; then
+        echo "Previous data found, Removing old header file directory path list..."
+        rm ./cmakeFileLists/externalHDirectoryList.txt
+    fi
+    echo "Grabbing all dir paths to our external libraries h/hpp files and throwing into ./cmakeFileLists/externalHDirectoryList.txt"
+    #grabs only the header dirs that correspond to our 3rd party libs
+    find . -type d -path "*External_Libraries*" | sort  &> ./cmakeFileLists/externalHDirectoryList.txt
+    FILES_UPDATED=1
+    echo "Finished fixing up the list that has all our external lib include dirs."
+else
+    echo "No changes in our external lib header directory list."
+fi
+echo ""
+
+
+echo "Script completed."
+echo ""
+if [ 0 = "${FILES_UPDATED}" ]; then 
+    echo "Files unchanged"
+else
+    echo "Files updated"
+fi
