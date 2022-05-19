44e2b61bbcd98b14eaa1b3cb516ca75ea871017d
Oliver <olgrant@uga.edu>
2022-05-19 10:55:29 +0200

    Typo in test example usage

--- tests/tests/019.test.newPDBClass.cpp
+++ tests/tests/019.test.newPDBClass.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
     if (argc != 2)
     {
         std::cout << "Usage: " << argv[0] << " inputFile.pdb\n";
-        std::cout << "Example: " << argv[0] << "tests/inputs/4mbz.pdb\n";
+        std::cout << "Example: " << argv[0] << " tests/inputs/4mbz.pdb\n";
         std::exit(EXIT_FAILURE);
     }
     pdb::PdbFile pdbFile(argv[1]);
