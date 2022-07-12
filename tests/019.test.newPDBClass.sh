178810859d3b43b59b83414e02cd5ef14f47773e
Oliver <olgrant@uga.edu>
2022-07-12 20:52:03 +0200

    Updates tests to new pattern with exit codes

--- tests/019.test.newPDBClass.sh
+++ tests/019.test.newPDBClass.sh
@@ -7,12 +7,15 @@ do
 	./newPdbClass tests/inputs/$file > output.txt
 	if !cmp  output.txt tests/correct_outputs/$file-output.txt > /dev/null 2>&1; then
     	printf "Test FAILED!. output.txt different from tests/correct_outputs/$file-output.txt\n Compare using diff\n"
+    	echo "Exit Code: 1" 
     	return 1;
 	elif !cmp  outputPdbFile.pdb tests/correct_outputs/$file-output.pdb > /dev/null 2>&1; then
 		printf "Test FAILED!. outputPdbFile.pdb different from tests/correct_outputs/$file-output.pdb\n Compare using diff or VMD\n"
+		echo "Exit Code: 1"
         return 1;
 	fi
 done
 printf "Test passed.\n"
 rm ./newPdbClass outputPdbFile.pdb output.txt
+echo "Exit Code: 0"
 return 0
