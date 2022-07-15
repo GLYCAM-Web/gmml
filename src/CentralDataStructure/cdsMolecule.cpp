70005fc7c66e49e6596d4a3f2981c595d2181016
Oliver <olgrant@uga.edu>
2022-07-15 17:45:38 +0200

    Deleting empty files

--- src/CentralDataStructure/cdsMolecule.cpp
+++
@@ -1,6 +0,0 @@
-//#include "includes/CentralDataStructure/cdsAtom.hpp"
-//#include "includes/CentralDataStructure/cdsResidue.hpp"
-//#include "includes/CentralDataStructure/cdsMolecule.hpp"
-//
-//using cds::cdsMolecule;
-//
--- src/CentralDataStructure/cdsResidue.cpp
+++
@@ -1,5 +0,0 @@
-//#include "includes/CentralDataStructure/cdsResidue.hpp"
-//#include "includes/CentralDataStructure/cdsAtom.hpp"
-//
-//using cds::cdsResidue;
-//
