3c7bd925ff89c45b8683f570f755093b66eee638
Oliver <olgrant@uga.edu>
2022-07-29 12:49:11 +0200

    Bugfix-UpdateTestsToCompileC++17

--- tests/011.test.writeResNumbers.sh
+++ tests/011.test.writeResNumbers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "Testing 011.writeResNumbers.cc (write original and new residue numbers into a PDB file)... "
-g++ -std=c++0x -I $GEMSHOME/gmml/ -L$GEMSHOME/gmml/bin/ -Wl,-rpath,$GEMSHOME/gmml/bin/ tests/011.writeResNumbers.cc -lgmml -pthread -o writeResNumbers
+g++ -std=c++17 -I $GEMSHOME/gmml/ -L$GEMSHOME/gmml/bin/ -Wl,-rpath,$GEMSHOME/gmml/bin/ tests/011.writeResNumbers.cc -lgmml -pthread -o writeResNumbers
 ./writeResNumbers tests/inputs/pdb2glycam_4YG0.pdb > 011.output_writeResidueNumbers.txt
 if [ -f 011.output.newNumbers.pdb ] && [ -f 011.output.original.pdb ]; then
     if ! cmp 011.output.newNumbers.pdb tests/correct_outputs/011.output.newNumbers.pdb > /dev/null 2>&1; then
