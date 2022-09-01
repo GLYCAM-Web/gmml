c6312130fa897c9f94cbbcd628e8fa61086aac32
Oliver <olgrant@uga.edu>
2022-09-01 13:30:13 +0200

    Generic writePdb function in cdsAtom class.

--- includes/CentralDataStructure/cdsAtom.hpp
+++ includes/CentralDataStructure/cdsAtom.hpp
@@ -40,6 +40,16 @@ public:
 	//////////////////////////////////////////////////////////
 	//                       DISPLAY FUNCTION               //
 	//////////////////////////////////////////////////////////
+    void WritePdb(std::ostream &stream,
+        		std::string residueName = " ",
+    			std::string residueNumber = " ",
+    			std::string recordName = "ATOM",
+    			std::string chainId = " ",
+    			std::string insertionCode = " ",
+    			std::string alternativeLocation = " ",
+    			std::string occupancy = " ",
+    			std::string temperatureFactor = " "
+    			) const;
 private:
 	//////////////////////////////////////////////////////////
 	//                       ATTRIBUTES                     //
@@ -103,6 +113,37 @@ bool cdsAtom<atomT>::isWithinBondingDistance(const atomT* otherAtom) const
     }
     return false;
 }
-
+//////////////////////////////////////////////////////////
+//                       DISPLAY FUNCTION               //
+//////////////////////////////////////////////////////////
+template <class atomT>
+void cdsAtom<atomT>::WritePdb(std::ostream &stream,
+		std::string residueName,
+		std::string residueNumber,
+		std::string recordName,
+		std::string chainId,
+		std::string insertionCode,
+		std::string alternativeLocation,
+		std::string occupancy,
+		std::string temperatureFactor
+		) const
+{
+	stream << std::left << std::setw(6) << recordName;
+	stream << std::right << std::setw(5) << this->getIndex() << std::left << std::setw(1) << " ";
+	stream << std::left << std::setw(4) << this->getName();
+	stream << std::left << std::setw(1) << alternativeLocation;
+    stream << std::right << std::setw(3) << residueName << std::left << std::setw(1) << " ";
+    stream << std::left << std::setw(1) << chainId;
+    stream << std::right << std::setw(4) << residueNumber;
+    stream << std::left << std::setw(1) << insertionCode << std::left << std::setw(3) << " ";
+    stream << std::right << std::setw(8) << std::fixed << std::setprecision(3) << this->getCoordinate()->GetX();
+    stream << std::right << std::setw(8) << std::fixed << std::setprecision(3) << this->getCoordinate()->GetY();
+    stream << std::right << std::setw(8) << std::fixed << std::setprecision(3) << this->getCoordinate()->GetZ();
+    stream << std::right << std::setw(6) << std::fixed << std::setprecision(2) << occupancy;
+    stream << std::right << std::setw(6) << std::fixed << std::setprecision(2) << temperatureFactor << std::left << std::setw(10) << " ";
+    stream << std::right << std::setw(2) << this->getElement();
+    stream << std::left << std::setw(2) << std::to_string(this->getCharge()) << std::endl;
+    return;
+}
 } // namespace
 #endif // CDSATOM_HPP
