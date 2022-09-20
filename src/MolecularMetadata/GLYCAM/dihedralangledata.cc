b43a231e62077fe01008b87c6db68fb3173ab56b
Oliver <olgrant@uga.edu>
2022-09-20 13:37:26 +0200

    Metadata for 1-5Araf linkages

--- src/MolecularMetadata/GLYCAM/dihedralangledata.cc
+++ src/MolecularMetadata/GLYCAM/dihedralangledata.cc
@@ -131,7 +131,10 @@ DihedralAngleDataContainer::DihedralAngleDataContainer()
         { "C.*"  , "O6"     , "Omg"  ,  60.0  ,  20.0  ,  20.0  , 1.0   , "permutation" , "gt" , 3 , 2 , {"none"}       , {"monosaccharide"}                  , "O6" , "C6" , "C5" , "O5"  },
         { "C.*"  , "O6"     , "Omg"  , 180.0  ,  20.0  ,  20.0  , 1.0   , "permutation" , "tg" , 3 , 3 , {"none"}       , {"gauche-effect=galacto"} , "O6" , "C6" , "C5" , "O5"  },
         { "C.*"  , "O6"     , "Omg"  , 180.0  ,  20.0  ,  20.0  , 0.001 , "permutation" , "tg" , 3 , 3 , {"none"}       , {"gauche-effect=gluco"}   , "O6" , "C6" , "C5" , "O5"  },
-        // 2-7 linkages copied from GlycamWeb Jan 2021. Branching in the linkage causes oddities with the bond number and which atoms are chosen for the torsion.
+		// Omega angle in x-5 linkages.
+		{ "C.*"  , "O5"     , "Omg"  , -60.0  ,  20.0  ,  20.0  , 1.0   , "permutation" , "gg" , 3 , 1 , {"none"}       , {"furanose"}                  , "O5" , "C5" , "C4" , "O4"  },
+		{ "C.*"  , "O5"     , "Omg"  ,  60.0  ,  20.0  ,  20.0  , 1.0   , "permutation" , "gt" , 3 , 2 , {"none"}       , {"furanose"}                  , "O5" , "C5" , "C4" , "O4"  },
+		// 2-7 linkages copied from GlycamWeb Jan 2021. Branching in the linkage causes oddities with the bond number and which atoms are chosen for the torsion.
         { "C2"   , "O7"     , "Phi"  , -60.0  ,  20.0  ,  20.0  , 1.0   , "permutation" , "-g" , 1 , 2 , {"ulosonate", "alpha"}  , {"ulosonate"}    , "C3" , "C2" , "O." , "C."  },
         { "C2"   , "O7"     , "Psi"  ,   0.0  ,  20.0  ,  20.0  , 1.0   , "permutation" , "c"  , 2 , 1 , {"ulosonate", "alpha"}  , {"ulosonate"}    , "C." , "O." , "C." , "H."  },
         { "C.*"  , "O7"     , "Omg7" ,  60.0  ,  20.0  ,  20.0  , 1.0   , "permutation" , "g"  , 3 , 1 , {"none"}                , {"ulosonate"}    , "O7" , "C7" , "C6" , "O6"  },
