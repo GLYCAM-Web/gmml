8d80e1e319abfe53f7e1ee7fcf6c30f3f3081788
Oliver <olgrant@uga.edu>
2022-10-06 14:40:46 +0200

    New file for storing only constants.
    Move whatever is used from common in here.

---
+++ includes/CodeUtils/constants.hpp
@@ -0,0 +1,9 @@
+#ifndef GMML_INCLUDES_CODEUTILS_CONSTANTS_HPP
+#define GMML_INCLUDES_CODEUTILS_CONSTANTS_HPP
+
+namespace codeUtils
+{
+const double CARBON_SURFACE_AREA = 36.31681103;
+const double PI_RADIAN = 4.0*atan(1.0);
+const double PI_DEGREE = 180.0;
+}// namespace
