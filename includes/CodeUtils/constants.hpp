15a1ec61c014f520881f24ede69c5242b68571be
Oliver <olgrant@uga.edu>
2022-10-20 14:10:14 +0200

    Moved the xNotSet constants into CodeUtils

--- includes/CodeUtils/constants.hpp
+++ includes/CodeUtils/constants.hpp
@@ -6,5 +6,8 @@ namespace codeUtils
 const double CARBON_SURFACE_AREA = 36.31681103;
 const double PI_RADIAN = 4.0*atan(1.0);
 const double PI_DEGREE = 180.0;
+const double dNotSet = 123456789.0;
+const int iNotSet = -123456;
+const std::string sNotSet = "?";
 }// namespace
 #endif
