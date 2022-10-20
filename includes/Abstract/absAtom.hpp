a7aacd327bcfb9c4218028992b06aef3df542dc8
Oliver <olgrant@uga.edu>
2022-10-20 14:09:43 +0200

    Added a number field to Atom.
    Not sure if I want this here or in an abstract class

--- includes/Abstract/absAtom.hpp
+++ includes/Abstract/absAtom.hpp
@@ -6,7 +6,7 @@
 #include <vector>
 
 #include "includes/GeometryTopology/coordinate.hpp"
-#include "includes/common.hpp" // dNotSet
+#include "includes/CodeUtils/constants.hpp" // dNotSet
 
 using GeometryTopology::Coordinate;
 namespace Abstract
@@ -25,11 +25,13 @@ public:
     const Coordinate* getCoordinate() const;
     inline double getCharge() const { return charge_;}
     inline std::string getType() const {return atomType_;}
+    inline int getNumber() const {return number_;}
     //////////////////////////////////////////////////////////
     //                       MUTATOR                        //
     //////////////////////////////////////////////////////////
     inline void setCharge(const double c) {charge_ = c;}
     inline void setType(const std::string s) {atomType_ = s;}
+    inline void setNumber(const int i) {number_ = i;}
     void setCoordinate(const Coordinate& c);
     void addCoordinate(const Coordinate& c);
     //////////////////////////////////////////////////////////
@@ -44,8 +46,9 @@ private:
     //                       ATTRIBUTES                     //
     //////////////////////////////////////////////////////////
     std::vector<std::unique_ptr<Coordinate>> coordinates_;     /*!< Position of the atom >*/
-    double charge_ = gmml::dNotSet;
+    double charge_ = codeUtils::dNotSet;
     std::string atomType_ = " ";
+    int number_ = codeUtils::iNotSet;
 };
 }
 #endif // INCLUDES_ABSTRACT_ATOM_HPP
