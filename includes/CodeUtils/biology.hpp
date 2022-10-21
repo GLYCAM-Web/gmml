ba2728e2d5a8902787ce789d11ff631d826c6e24
Oliver <olgrant@uga.edu>
2022-10-21 16:49:06 +0200

    Breaking up common.hpp into logical include-able parts

---
+++ includes/CodeUtils/biology.hpp
@@ -0,0 +1,26 @@
+#ifndef GMML_INCLUDES_CODEUTILS_BIOLOGY_HPP
+#define GMML_INCLUDES_CODEUTILS_BIOLOGY_HPP
+
+#include <string>
+#include <vector>
+
+namespace biology
+{
+const std::vector<std::string> proteinResidueNames = { "ALA", "ASP", "ASN", "ARG", "GLY",
+            "GLU", "GLN", "PRO", "HIS", "HIP","CYS",
+            "VAL", "LEU", "THR", "SER", "LYS",
+            "MET", "MSE", "TYR", "TRP", "PHE", "SEC",
+            "ILE", "CYX", "HID", "HIE", "NLN",
+            "OLY", "OLS", "OLT", "NALA", "NASP",
+"NASN", "NARG", "NGLY", "NGLU", "NGLN",
+"NPRO", "NHIS", "NCYS", "NVAL", "NLEU",
+"NTHR", "NSER", "NLYS", "NMET", "NTYR",
+"NTRP", "NPHE", "NSEC", "NILE", "NCYX",
+"NHID", "NHIE", "CNALA", "CNASP",
+            "CASN", "CARG", "CGLY", "CGLU", "CGLN",
+            "CPRO", "CHIS", "CCYS", "CVAL", "CLEU",
+            "CTHR", "CSER", "CLYS", "CMET", "CTYR",
+            "CTRP", "CPHE", "CSEC", "CILE", "CCYX",
+            "CHID", "CHIE"};
+}// namespace
+#endif
