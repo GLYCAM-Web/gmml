baff97af973794c3f2fd33a91f9d2447626d0560
Oliver <olgrant@uga.edu>
2022-11-01 13:28:51 +0100

    Heavy edits including debugging deoxy and derivative handling.

--- src/InputSet/CondensedSequence/carbohydrate.cpp
+++ src/InputSet/CondensedSequence/carbohydrate.cpp
@@ -31,23 +31,43 @@ Carbohydrate::Carbohydrate(std::string inputSequence, std::string prepFilePath) 
 	    std::cout << "it's glycam name is " << this->GetGlycamResidueName(parsedResidue) << std::endl;
         std::cout << "it's label is " << parsedResidue->getLabel() << std::endl;
         std::cout << parsedResidue->PrintToString() << std::endl;
-	    cds::Residue* prepResidue = glycamPrepFileSelect.getResidue(this->GetGlycamResidueName(parsedResidue));
-	    if (prepResidue == nullptr)
-	    {
-	        std::string message = "Did not find prep entry for " + parsedResidue->getName() + " with glycam residue code: " + this->GetGlycamResidueName(parsedResidue);
-	        gmml::log(__LINE__,__FILE__,gmml::ERR, message);
-	        throw(std::runtime_error(message));
-	    }
-	    std::cout << "prepResidue is " << prepResidue->getName() << std::endl;
-	    // ParsedResidue has the correct residue connectivities, prepResidue has the atoms.
-	    parsedResidue->setAtoms(prepResidue->extractAtoms()); // This moves the atoms, i.e. for prepResidue "Moved from objects are left in a valid but unspecified state"
-	    glycamPrepFileSelect.deleteResidue(prepResidue); // Death to the prepResidue, if there are repeats with the same name, the next search would find the one without atoms.
-        std::cout << "Finished moving atoms from prepResidue to parsed Residue. Adventure awaits! Huzzah!" << std::endl;
+        // Deoxy
         if (parsedResidue->GetType() == Abstract::ResidueType::Deoxy)
         {
             std::cout << "Dealing with deoxy for " << parsedResidue->getName() << "\n";
             gmml::log(__LINE__, __FILE__, gmml::INF, "Dealing with deoxy for " + parsedResidue->getName());
-            parsedResidue->MakeDeoxy(parsedResidue->GetLink());
+            cds::Residue* residueToBeDeoxified = parsedResidue->getParents().at(0);
+            residueToBeDeoxified->MakeDeoxy(parsedResidue->GetLink());
+            this->deleteResidue(parsedResidue); // Remove the deoxy derivative now.
+        }
+        else // Not Deoxy
+        {
+            cds::Residue* prepResidue = glycamPrepFileSelect.getResidue(this->GetGlycamResidueName(parsedResidue));
+            if (prepResidue == nullptr)
+            {
+                std::string message = "Did not find prep entry for " + parsedResidue->getName() + " with glycam residue code: " + this->GetGlycamResidueName(parsedResidue);
+                gmml::log(__LINE__,__FILE__,gmml::ERR, message);
+                throw(std::runtime_error(message));
+            }
+            std::cout << "prepResidue is " << prepResidue->getName() << std::endl;
+            // ParsedResidue has the correct residue connectivities, prepResidue has the atoms.
+            parsedResidue->setAtoms(prepResidue->extractAtoms()); // This moves the atoms, i.e. for prepResidue "Moved from objects are left in a valid but unspecified state"
+            glycamPrepFileSelect.deleteResidue(prepResidue); // Death to the prepResidue, if there are repeats with the same name, the next search would find the one without atoms.
+            std::cout << "Finished moving atoms from prepResidue to parsed Residue. Adventure awaits! Huzzah!" << std::endl;
+            // Charge adjustment. If residue is Derivaitve, the parent residue connection atom's charge is adjusted.
+            if (parsedResidue->GetType() == Abstract::ResidueType::Derivative)
+            {
+                std::stringstream ss;
+                ss << "Charge Adjustment.\n";
+                gmml::MolecularMetadata::GLYCAM::Glycam06DerivativeChargeAdjustmentLookupContainer lookup;
+                std::string adjustAtomName = lookup.GetAdjustmentAtom(parsedResidue->getName());
+                adjustAtomName += parsedResidue->GetLinkageName().substr(0,1);
+                cds::Atom* atomToAdjust = parsedResidue->getParents().at(0)->FindAtom(adjustAtomName);
+                ss << "    Derivative is " << parsedResidue->getName() << ". Adjusting charge on " << atomToAdjust->getName() << "\n";
+                ss << "    Adjusting by: " << lookup.GetAdjustmentCharge(parsedResidue->getName()) << "\n";
+                gmml::log(__LINE__, __FILE__, gmml::INF, ss.str());
+                atomToAdjust->setCharge(atomToAdjust->getCharge() + lookup.GetAdjustmentCharge(parsedResidue->getName()) );
+            }
         }
 	}
     std::cout << "\n\n\nOn to setting 3d structure!\n\n";
@@ -174,6 +194,7 @@ void Carbohydrate::BondResiduesDeduceAtoms(cds::Residue* childResidue, cds::Resi
 		    throw std::runtime_error(message);
 		}
 		parentAtom = TemplatedSelections::getNonCarbonHeavyAtomNumbered(parentResidue->getAtoms(), linkageLabel.substr(linkPosition));
+		std::cout << "Parent atom is " << parentAtom->getName() << std::endl;
 	}
 	else
 	{
@@ -192,7 +213,8 @@ void Carbohydrate::BondResiduesDeduceAtoms(cds::Residue* childResidue, cds::Resi
 	if (childResidue->GetType() == ResidueType::Derivative)
 	{
 		gmml::MolecularMetadata::GLYCAM::Glycam06DerivativeAglyconeConnectionAtomLookup connectionAtomLookup;
-		childAtomName = connectionAtomLookup.GetConnectionAtomForResidue(childResidue->getName());
+		std::string glycamNameForResidue = this->GetGlycamResidueName(static_cast<ParsedResidue*>(childResidue));
+		childAtomName = connectionAtomLookup.GetConnectionAtomForResidue(glycamNameForResidue);
 	}
 	else if (childResidue->GetType() == ResidueType::Sugar)
 	{
@@ -214,32 +236,19 @@ void Carbohydrate::BondResiduesDeduceAtoms(cds::Residue* childResidue, cds::Resi
 	Atom* childAtom = childResidue->FindAtom(childAtomName);
 	if (childAtom == nullptr)
 	{
-	    std::string message = "Did not find atom named " + childAtomName + " in residue: " + childResidue->getId();
+	    std::string message = "Did not find child atom named " + childAtomName + " in child residue: " + childResidue->getId();
 	    gmml::log(__LINE__, __FILE__, gmml::ERR, message);
 	    throw std::runtime_error(message);
 	}
     gmml::log(__LINE__,__FILE__,gmml::INF, childAtom->getId());
-	// Now bond the atoms. Needs to change when AtomNode goes away.
-	childAtom->addBond(parentAtom); // parentAtom also connected to childAtom. Fancy.
-	std::stringstream logss;
-	logss << "Bonded " << parentResidue->getName() << "@" << parentAtom->getName() << " to " << childResidue->getName() << "@" << childAtomName << std::endl;
-	// Charge adjustment
-	if (childResidue->GetType() == ResidueType::Derivative)
-	{
-		logss << "Charge Adjustment.\n";
-		gmml::MolecularMetadata::GLYCAM::Glycam06DerivativeChargeAdjustmentLookupContainer lookup;
-		std::string adjustAtomName = lookup.GetAdjustmentAtom(childResidue->getName());
-		adjustAtomName += linkageLabel.substr(0,1);
-		Atom* atomToAdjust = parentResidue->FindAtom(adjustAtomName);
-		logss << "    Derivative is " << childResidue->getName() << ". Adjusting charge on " << atomToAdjust->getName() << "\n";
-		logss << "    Adjusting by: " << lookup.GetAdjustmentCharge(childResidue->getName()) << "\n";
-		gmml::log(__LINE__, __FILE__, gmml::INF, logss.str());
-		atomToAdjust->setCharge(atomToAdjust->getCharge() + lookup.GetAdjustmentCharge(childResidue->getName()) );
-	}
+    std::stringstream logss;
 	// Geometry
-	logss << "Setting bond distance between " << parentAtom->getName() << " and " << childAtom->getName() << ".\n";
+	logss << "Setting bond distance between parent " << parentAtom->getName() << " and child " << childAtom->getName() << ".\n";
 	gmml::log(__LINE__, __FILE__, gmml::INF, logss.str());
 	GeometryTopology::FindAtomsToMoveSetDistance(parentAtom, childAtom);
+	//   Now bond the atoms. This could also set distance?, and angle? if passed to function?
+	childAtom->addBond(parentAtom); // parentAtom also connected to childAtom. Fancy.
+	logss << "Bonded " << parentResidue->getName() << "@" << parentAtom->getName() << " to " << childResidue->getName() << "@" << childAtomName << std::endl;
 	// Angle
 	logss << "Setting angles.\n";
 	gmml::log(__LINE__, __FILE__, gmml::INF, logss.str());
@@ -248,9 +257,12 @@ void Carbohydrate::BondResiduesDeduceAtoms(cds::Residue* childResidue, cds::Resi
 	{
 		if ( (parentAtomNeighbor->getName().at(0) != 'H') && (parentAtomNeighbor != childAtom ) )
 		{
-			GeometryTopology::SetAngle(parentAtomNeighbor->getCoordinate(), parentAtom->getCoordinate(), childAtom->getCoordinate(), angle_to_set, parentResidue->getCoordinates());
+		   // std::cout << "Setting angle between parentNeighbor " << parentAtomNeighbor->getName() << ", parent " << parentAtom->getName() << " and child " << childAtom->getName() << ". child residue " << childResidue->getName() << " will move\n";
+			GeometryTopology::SetAngle(parentAtomNeighbor->getCoordinate(), parentAtom->getCoordinate(), childAtom->getCoordinate(), angle_to_set, childResidue->getCoordinates());
+			break;
 		}
 	}
+
 	return;
 }
 
@@ -260,8 +272,11 @@ std::vector<std::string> Carbohydrate::GetGlycamNamesOfResidues() const
     std::cout << "Glycam names are: ";
 	for(auto &residue : this->getResidues())
 	{
-	    names.push_back(this->GetGlycamResidueName(static_cast<ParsedResidue*>(residue)));
-	    std::cout << names.back();
+	    if (residue->GetType() != Abstract::ResidueType::Deoxy)
+	    {
+	        names.push_back(this->GetGlycamResidueName(static_cast<ParsedResidue*>(residue)));
+	        std::cout << names.back() << ", ";
+	    }
 	}
 	std::cout << "\n";
 	return names;
@@ -270,6 +285,11 @@ std::vector<std::string> Carbohydrate::GetGlycamNamesOfResidues() const
 
 std::string Carbohydrate::GetGlycamResidueName(ParsedResidue *residue) const
 {
+    if (residue->GetType() == Abstract::ResidueType::Deoxy)
+    {
+        gmml::log(__LINE__, __FILE__, gmml::WAR, "Bad idea: We asked for Glycam Residue Name of a deoxy type residuw with name: " + residue->GetResidueName());
+        return "";
+    }
     std::string linkages = "";
     if (residue->GetType() == Abstract::ResidueType::Sugar)
     {
