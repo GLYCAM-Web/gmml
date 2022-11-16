5477a353db804f563aef1e5a69685e4d07ad62c2
Oliver <olgrant@uga.edu>
2022-11-16 09:49:16 +0100

    Somehow this worked without math.h, but not anymore.

--- includes/CodeUtils/constants.hpp
+++ includes/CodeUtils/constants.hpp
@@ -1,13 +1,18 @@
 #ifndef GMML_INCLUDES_CODEUTILS_CONSTANTS_HPP
 #define GMML_INCLUDES_CODEUTILS_CONSTANTS_HPP
+#include <math.h> // atan
 
 namespace codeUtils
 {
+
+const double DEFAULT_ANGLE = 109.4;
 const double CARBON_SURFACE_AREA = 36.31681103;
 const double PI_RADIAN = 4.0*atan(1.0);
 const double PI_DEGREE = 180.0;
 const double dNotSet = 123456789.0;
 const int iNotSet = -123456;
 const std::string sNotSet = "?";
+
 }// namespace
+
 #endif
