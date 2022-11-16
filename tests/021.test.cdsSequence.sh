848cf708f3e359bfc0cf4ae944feece364ed010a
Oliver <olgrant@uga.edu>
2022-11-16 09:57:18 +0100

    Added a new test for the new sequence class

---
+++ tests/021.test.cdsSequence.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+printf "Testing 021.test.cdsSequence.cpp... "
+g++ -std=c++17 -I $GEMSHOME/gmml/ -I $GEMSHOME/gmml/ -L$GEMSHOME/gmml/bin/ -Wl,-rpath,$GEMSHOME/gmml/bin/ tests/021.test.cdsSequence.cpp -lgmml -pthread -o cdsSequence
+./cdsSequence > 021.output_cdsSequence.txt 2>&1
+if ! cmp  021.output_cdsSequence.txt tests/correct_outputs/021.output_cdsSequence.txt > /dev/null 2>&1; then
+    printf "Test FAILED! Output file different\n"
+    echo "Exit Code: 1"
+    return 1
+else
+    printf "Test passed.\n"
+    rm cdsSequence 021.output_cdsSequence.txt 
+    echo "Exit Code: 0"
+    return 0
+fi
+
