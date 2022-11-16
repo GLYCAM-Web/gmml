fc44c36ac1caa3c4a34b9c94143bf6386bf63a50
Oliver <olgrant@uga.edu>
2022-11-16 09:53:37 +0100

    Not using the generic isElementPresent function anymore

--- src/Abstract/absResidue.cpp
+++ src/Abstract/absResidue.cpp
@@ -5,10 +5,10 @@ using Abstract::absResidue;
 
 Abstract::ResidueType absResidue::determineType(const std::string &residueName) const
 {
-	if ( codeUtils::isElementPresent(biology::proteinResidueNames.begin(), biology::proteinResidueNames.end(), residueName) )
-	{
-		return ResidueType::Protein;
-	}
-	// Do we want to figure out solvent, aglycone etc here too? Maybe.
-	return ResidueType::Undefined;
+    if ( std::find(biology::proteinResidueNames.begin(), biology::proteinResidueNames.end(), residueName)  != biology::proteinResidueNames.end() )
+    {
+        return ResidueType::Protein;
+    }
+    // ToDo we want to figure out solvent, aglycone etc here too?.
+    return ResidueType::Undefined;
 }
