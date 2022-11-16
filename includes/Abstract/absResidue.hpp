82342a5a5101d3ba7900c8d745642f6b4cad4494
Oliver <olgrant@uga.edu>
2022-11-16 09:36:34 +0100

    Reordered the enum so protein is first. Fixed indentation.

--- includes/Abstract/absResidue.hpp
+++ includes/Abstract/absResidue.hpp
@@ -5,28 +5,28 @@
 
 namespace Abstract
 {
-    enum ResidueType {Aglycone, Sugar, Derivative, Solvent, Protein, Deoxy, Undefined};
-	class absResidue 
-	{
-	public:
-        //enum Type {Aglycone, Sugar, Derivative, Solvent, Protein, Deoxy, Undefined};
-        //////////////////////////////////////////////////////////
-        //                       ACCESSOR                       //
-        //////////////////////////////////////////////////////////
-        inline ResidueType GetType() const {return type_;}
-        //////////////////////////////////////////////////////////
-        //                       MUTATOR                        //
-        //////////////////////////////////////////////////////////
-        inline void SetType(ResidueType type) {type_ = type;}
-        //////////////////////////////////////////////////////////
-        //                       FUNCTIONS                      //
-        //////////////////////////////////////////////////////////
-        ResidueType determineType(const std::string &residueName) const;
-    private:      
-        //////////////////////////////////////////////////////////
-        //                       ATTRRIBUTES                    //
-        //////////////////////////////////////////////////////////
-        ResidueType type_ = Undefined;  // enum Type. See enum above.
-	};
+enum ResidueType {Protein, Sugar, Aglycone, Derivative, Solvent, Deoxy, Undefined};
+class absResidue
+{
+public:
+    //enum Type {Aglycone, Sugar, Derivative, Solvent, Protein, Deoxy, Undefined};
+    //////////////////////////////////////////////////////////
+    //                       ACCESSOR                       //
+    //////////////////////////////////////////////////////////
+    inline ResidueType GetType() const {return type_;}
+    //////////////////////////////////////////////////////////
+    //                       MUTATOR                        //
+    //////////////////////////////////////////////////////////
+    inline void SetType(ResidueType type) {type_ = type;}
+    //////////////////////////////////////////////////////////
+    //                       FUNCTIONS                      //
+    //////////////////////////////////////////////////////////
+    ResidueType determineType(const std::string &residueName) const;
+private:
+    //////////////////////////////////////////////////////////
+    //                       ATTRRIBUTES                    //
+    //////////////////////////////////////////////////////////
+    ResidueType type_ = Undefined;  // enum Type. See enum above.
+};
 }
 #endif
