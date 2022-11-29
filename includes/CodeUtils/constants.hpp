30bdfc9fcefaf016ce651659300ca8b128fb90c8
Oliver <olgrant@uga.edu>
2022-11-29 12:46:32 +0100

    Bugfix, doesn't work unless strings was already known

--- includes/CodeUtils/constants.hpp
+++ includes/CodeUtils/constants.hpp
@@ -1,10 +1,11 @@
 #ifndef GMML_INCLUDES_CODEUTILS_CONSTANTS_HPP
 #define GMML_INCLUDES_CODEUTILS_CONSTANTS_HPP
+
 #include <math.h> // atan
+#include <string>
 
 namespace constants
 {
-
 const double residueDistanceOverlapCutoff = 9.0;
 const double maxCutOff = 1.65; // ToDo this value seems low if checking distance for bonding?
 const double DEFAULT_ANGLE = 109.4;
@@ -15,7 +16,5 @@ const double PI_DEGREE = 180.0;
 const double dNotSet = 123456789.0;
 const int iNotSet = -123456;
 const std::string sNotSet = "?";
-
 }// namespace
-
 #endif
