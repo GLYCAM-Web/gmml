314e52715c58d9555e14da12cb1515e9c66d09de
Oliver <olgrant@uga.edu>
2022-12-13 12:21:05 +0100

    Cleaned up graphVizDotConfig struct. Can also pass in directory now.

--- includes/InputSet/CondensedSequence/graphVizDotConfig.hpp
+++ includes/InputSet/CondensedSequence/graphVizDotConfig.hpp
@@ -5,21 +5,13 @@ namespace CondensedSequence
 {
 struct GraphVizDotConfig
 {
-    bool show_config_labels_;
-    bool show_edge_labels_;
-    bool show_position_labels_;
-    int dpi_;
-    std::string file_name_;
-    std::string svg_directory_path_;
-    GraphVizDotConfig()
-    {
-        this->show_edge_labels_ = false;
-        this->show_config_labels_ = true;
-        this->show_position_labels_ = true;
-        this->dpi_ = 72;
-        this->svg_directory_path_ = "/programs/gems/gmml/includes/MolecularMetadata/Sugars/SNFG_Symbol_Images/";
-        this->file_name_ = "oligosaccharide.dot";
-    }
+    GraphVizDotConfig(std::string svg_directory_path = "/programs/gems/gmml/includes/MolecularMetadata/Sugars/SNFG_Symbol_Images/") :  svg_directory_path_(svg_directory_path) {}
+    bool show_config_labels_ = true;
+    bool show_edge_labels_ = false;
+    bool show_position_labels_ = true;
+    int dpi_ = 72;
+    std::string file_name_ = "oligosaccharide.dot";
+    std::string svg_directory_path_ = "/programs/gems/gmml/includes/MolecularMetadata/Sugars/SNFG_Symbol_Images/";
 };
 } // namespace
 #endif /* INCLUDES_INPUTSET_CONDENSEDSEQUENCE_GRAPHVIZDOTCONFIG_HPP_ */
