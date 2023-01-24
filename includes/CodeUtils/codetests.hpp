fcb726949faa20750ba1626fa25f259461568ea0
Oliver <olgrant@uga.edu>
2023-01-24 12:13:32 +0100

    Copied metropolish criterion code into CodeUtils

--- includes/CodeUtils/codetests.hpp
+++ includes/CodeUtils/codetests.hpp
@@ -1,6 +1,6 @@
 #ifndef CODE_UTILS_HPP
 #define CODE_UTILS_HPP
-
+// ToDo This belongs in gmml/tests, doesn't need to include common and can be a function instead of a class.
 #include "../common.hpp"
 
 namespace CodeUtils
