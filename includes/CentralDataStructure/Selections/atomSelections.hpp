ff439c6c8d3123800daaf55c8cec2781f1dc550d
Oliver <olgrant@uga.edu>
2023-01-31 11:10:34 +0100

    Selection functionality for glycoprotein builder

--- includes/CentralDataStructure/Selections/atomSelections.hpp
+++ includes/CentralDataStructure/Selections/atomSelections.hpp
@@ -1,6 +1,7 @@
 #ifndef INCLUDES_CENTRALDATASTRUCTURE_SELECTIONS_ATOMSELECTIONS_HPP_
 #define INCLUDES_CENTRALDATASTRUCTURE_SELECTIONS_ATOMSELECTIONS_HPP_
 
+#include "includes/CentralDataStructure/coordinate.hpp"
 #include "includes/CentralDataStructure/atom.hpp"
 #include "includes/CentralDataStructure/residue.hpp"
 #include "includes/CodeUtils/templatedSelections.hpp"
@@ -8,12 +9,20 @@
 #include <string>
 #include <algorithm> //find
 
+using cds::Atom;
+using cds::Residue;
+
 namespace cdsSelections
 {
-cds::Atom* getNonCarbonHeavyAtomNumbered(std::vector<cds::Atom*> atoms, const std::string queryNumber);
-void FindConnectedAtoms(std::vector<cds::Atom*> &visitedAtoms, cds::Atom* currentAtom);
-cds::Atom* getNeighborNamed(const cds::Atom* queryAtom, const std::string neighborName);
+Atom* getNonCarbonHeavyAtomNumbered(std::vector<Atom*> atoms, const std::string queryNumber);
+void FindConnectedAtoms(std::vector<Atom*> &visitedAtoms, Atom* currentAtom);
+Atom* getNeighborNamed(const Atom* queryAtom, const std::string neighborName);
+Atom* selectNeighborNotInAtomVector(const Atom* atomWithNeighbors, std::vector<Atom*> queryAtoms);
+Atom* guessAnomericAtom(Residue* queryResidue);
+std::vector<Coordinate*> getCoordinates(std::vector<Atom*> queryAtoms);
+unsigned long int CountInternalHeavyAtomBonds(std::vector<Atom*> queryAtoms);
+std::vector<Atom*> FindHeavyAtoms(std::vector<Atom*> queryAtoms);
+unsigned long int CountAtomsWithinBondingDistance(const Atom* queryAtom, std::vector<Atom*> otherAtoms);
+std::vector<Atom*> FindAtomsWithinDistance(const Atom* queryAtom, std::vector<Atom*> otherAtoms, double distance = 1.0);
 } // namespace
-
 #endif
-
--- src/CentralDataStructure/Selections/atomSelections.cpp
+++ src/CentralDataStructure/Selections/atomSelections.cpp
@@ -1,6 +1,7 @@
 #include "includes/CentralDataStructure/Selections/atomSelections.hpp"
+#include "includes/CodeUtils/logging.hpp"
 
-cds::Atom* cdsSelections::getNonCarbonHeavyAtomNumbered(std::vector<cds::Atom*> atoms, const std::string queryNumber)
+Atom* cdsSelections::getNonCarbonHeavyAtomNumbered(std::vector<Atom*> atoms, const std::string queryNumber)
 {
     for (auto &atom : atoms)
     {    // Assumes atom names like C2, O2 or N2. Nothing else should match.
@@ -17,7 +18,7 @@ cds::Atom* cdsSelections::getNonCarbonHeavyAtomNumbered(std::vector<cds::Atom*> 
     return nullptr;
 }
 
-void cdsSelections::FindConnectedAtoms(std::vector<cds::Atom*> &visitedAtoms, cds::Atom* currentAtom)
+void cdsSelections::FindConnectedAtoms(std::vector<Atom*> &visitedAtoms, Atom* currentAtom)
 {
     visitedAtoms.push_back(currentAtom);
     for(auto &neighbor : currentAtom->getNeighbors())
@@ -31,9 +32,102 @@ void cdsSelections::FindConnectedAtoms(std::vector<cds::Atom*> &visitedAtoms, cd
 }
 
 // ToDo ensure all nodes have names, do this up there.
-
-cds::Atom* cdsSelections::getNeighborNamed(const cds::Atom* queryAtom, const std::string neighborName)
+Atom* cdsSelections::getNeighborNamed(const Atom* queryAtom, const std::string neighborName)
 {
     return codeUtils::findElementWithName(queryAtom->getNeighbors(), neighborName);
 }
 
+// Step 1. If the C1 atom has a neighbor that isn't in the queryResidue, return C1.
+// Step 2. If the C2 atom has a neighbor that isn't in the queryResidue, return C2.
+// Step 3. Panic.
+Atom* cdsSelections::guessAnomericAtom(cds::Residue* queryResidue)
+{
+    std::vector<std::string> usualSuspects = {"C1", "C2"};
+    for (auto & suspectName : usualSuspects)
+    {
+        Atom *potentialAnomer = codeUtils::findElementWithName(queryResidue->getAtoms(), suspectName);
+        if(cdsSelections::selectNeighborNotInAtomVector(potentialAnomer, queryResidue->getAtoms()) != nullptr)
+        {
+            return potentialAnomer;
+        }
+    }
+    std::string message = "Did not find a C1 or C2 with a foreign neighbor in residue: " + queryResidue->getId() + ", thus no anomeric atom was found.";
+    gmml::log(__LINE__, __FILE__, gmml::ERR, message);
+    throw std::runtime_error(message);
+    return nullptr;
+}
+
+Atom* cdsSelections::selectNeighborNotInAtomVector(const Atom* atomWithNeighbors, std::vector<Atom*> queryAtoms)
+{
+    for (auto & neighbor : atomWithNeighbors->getNeighbors())
+    {
+        if( std::find(queryAtoms.begin(), queryAtoms.end(), neighbor) == queryAtoms.end())
+        {
+            return neighbor;
+        }
+    }
+    return nullptr;
+}
+
+std::vector<Coordinate*> cdsSelections::getCoordinates(std::vector<Atom*> queryAtoms)
+{
+    std::vector<Coordinate*> coords(queryAtoms.size());
+    for(auto & atom : queryAtoms)
+    {
+        coords.push_back(atom->getCoordinate());
+    }
+    return coords;
+}
+
+unsigned long int cdsSelections::CountInternalHeavyAtomBonds(std::vector<Atom*> queryAtoms)
+{
+    // This belong in a Molecule class. Algo:
+    // Get heavy atoms in molecule. Check number of neighbors. Check if there are more atoms within distance d than neighbors. Scream if so.
+    unsigned long int count = 0;
+    std::vector<Atom*> heavyAtoms = cdsSelections::FindHeavyAtoms(queryAtoms);
+    for (auto &atom : heavyAtoms)
+    {
+        count += cdsSelections::CountAtomsWithinBondingDistance(atom, heavyAtoms);
+    }
+    return count;
+}
+
+std::vector<Atom*> cdsSelections::FindHeavyAtoms(std::vector<Atom*> queryAtoms)
+{
+    std::vector<Atom*> foundAtoms;
+    std::vector<std::string> heavyList = {"C", "O", "N", "S", "P"};
+    for (auto &atom : queryAtoms)
+    {
+        if( std::find(heavyList.begin(), heavyList.end(), atom->getElement()) != heavyList.end())
+        {
+            foundAtoms.push_back(atom);
+        }
+    }
+    return foundAtoms;
+}
+
+unsigned long int cdsSelections::CountAtomsWithinBondingDistance(const Atom* queryAtom, std::vector<Atom*> otherAtoms)
+{
+    unsigned long int count;
+    for (auto &otherAtom : otherAtoms)
+    {
+        if (queryAtom->isWithinBondingDistance(otherAtom))
+        {
+            ++count;
+        }
+    }
+    return count;
+}
+
+std::vector<Atom*> cdsSelections::FindAtomsWithinDistance(const Atom* queryAtom, std::vector<Atom*> otherAtoms, double distance)
+{
+    std::vector<Atom*> foundAtoms;
+    for (auto &otherAtom : otherAtoms)
+    {
+        if (queryAtom->getCoordinate()->Distance(otherAtom->getCoordinate()) < distance)
+        {
+            foundAtoms.push_back(otherAtom);
+        }
+    }
+    return foundAtoms;
+}
