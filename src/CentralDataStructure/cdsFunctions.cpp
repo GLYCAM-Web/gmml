5f79098f7f07411be299aec63fc1ff4411da7842
Oliver <olgrant@uga.edu>
2023-01-31 11:09:45 +0100

    Adding serialize residue numbers functionality for glycoprotein builder

--- src/CentralDataStructure/cdsFunctions.cpp
+++ src/CentralDataStructure/cdsFunctions.cpp
@@ -9,3 +9,15 @@ void cds::serializeAtomNumbers(std::vector<cds::Atom*> atoms)
     }
     return;
 }
+
+
+void cds::serializeResidueAndAtomNumbers(std::vector<cds::Residue*> residues)
+{
+    unsigned int i = 0;
+    for(auto &residue : residues)
+    {
+        residue->setNumber(++i);
+        cds::serializeAtomNumbers(residue->getAtoms());
+    }
+    return;
+}
