a2d8e77155f64b731a718eed442612e3226bb108
Oliver <olgrant@uga.edu>
2023-04-19 14:20:55 +0200

    Cleaning

--- includes/CentralDataStructure/Selections/residueSelections.hpp
+++ includes/CentralDataStructure/Selections/residueSelections.hpp
@@ -5,29 +5,11 @@
 #include "includes/CentralDataStructure/molecule.hpp"
 #include <vector>
 
-//ToDo is this useful or would cdsResidue work fine?
 namespace cdsSelections
 {
 using cds::Residue;
-//template< class residueT>
-//inline typename std::vector<Residue*> selectResiduesByType(std::vector<Residue*> inputResidues, cds::ResidueType queryType)
 std::vector<Residue*> selectResiduesByType(std::vector<Residue*> inputResidues, cds::ResidueType queryType);
-//{
-//	std::vector<Residue*> selectedResidues;
-//	for(auto & residue : inputResidues)
-//	{
-//		if ( residue->GetType() == queryType )
-//		{
-//			selectedResidues.push_back(residue);
-//		}
-//	}
-//	return selectedResidues;
-//}
-//cds::Molecule* findMoleculeOfResidue(std::vector<cds::Molecule*> molecules, Residue* queryResidue);
 unsigned int findHighestResidueNumber(std::vector<Residue*> residues);
 Residue* FindNeighborResidueConnectedViaSpecificAtom(Residue* queryResidue, const std::string queryAtomName);
-
 } // namespace
-
-#endif // CDS SELECTIONS
-
+#endif
--- includes/CodeUtils/templatedSelections.hpp
+++ includes/CodeUtils/templatedSelections.hpp
@@ -4,28 +4,8 @@
 #include <vector>
 #include <algorithm> // find
 //ToDo move this to CentralDataStructure/Selections and rename the namespace.
-// Must pass in T in order for the return type to be deduced by the compiler. There may be a better way but I can't find it. Tried passing in T typed iterators, but no deal as the containers are templated too
-// This works fine, but the extra T thing makes it real ugly and you get warnings about it not being used.
-// My compromise is to use the function below, but it loses the ability to pass in iterators to any container type, now it's just vector.
-// Perhaps if I can figure out how to pass in a lamda?(nope, member functions need binding I think) indicating the member function to use e.g. getName vs getNumber, it could
-// then deduce the type of T from that and we could go back to this one, and further reduce code duplication.
-//template <class RandomAccessIterator, class T>
-//std::vector<T*> getElementsWithName( RandomAccessIterator first, RandomAccessIterator last, std::vector<std::string> queryNames, const T& value)
 namespace codeUtils
 {
-//template <class T> // Here type deduction is funky, because T can be both const and non-const in inputs, so it can't deduce properly. Couldn't find a reasonable solution.
-//bool isElementPresent( const std::vector<T>& inputVector, const T query )
-//{
-//    for (auto & element : inputVector)
-//    {
-//		if( element == query )
-//		{
-//			return true;
-//		}
-//	}
-//	return false;
-//}
-
 template <class T>
 std::vector<T*> getElementsWithNames(const std::vector<T*>& inputVector, const std::vector<std::string>& queryNames)
 {
@@ -38,20 +18,6 @@ std::vector<T*> getElementsWithNames(const std::vector<T*>& inputVector, const s
 		}
 	}
 	return results;
-}
-
-// ToDo: This const version may be unnecessary. Can pass in const types to the next one.
-template <class T>
-const T* findElementWithName(const std::vector<const T*>& inputVector, const std::string& queryName)
-{
-	for (const auto & element : inputVector)
-	{
-		if ( element->getName() == queryName )
-		{
-			return element;
-		}
-	}
-	return nullptr;
 }
 
 template <class T>
@@ -116,7 +82,5 @@ void findPathBetweenElementsInGraph(const T current, const T target, std::vector
         path.push_back(current);
     }
 }
-
 } // namespace
-
 #endif /* INCLUDES_CODEUTILS_TEMPLATEDSELECTIONS_HPP_ */
