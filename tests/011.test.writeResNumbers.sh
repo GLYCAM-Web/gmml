4f30b9bfd2cf3db4c603562f948269223acc291f
Oliver <olgrant@uga.edu>
2023-04-20 13:47:02 +0200

    Moved pdbToGlycam into internalPrograms. Improved naming.

--- tests/011.test.writeResNumbers.sh
+++ tests/011.test.writeResNumbers.sh
@@ -2,7 +2,7 @@
 
 printf "Testing 011.writeResNumbers.cc (write original and new residue numbers into a PDB file)... "
 g++ -std=c++17 -I $GEMSHOME/gmml/ -L$GEMSHOME/gmml/bin/ -Wl,-rpath,$GEMSHOME/gmml/bin/ tests/011.writeResNumbers.cc -lgmml -pthread -o writeResNumbers
-./writeResNumbers tests/inputs/pdb2glycam_4YG0.pdb > 011.output_writeResidueNumbers.txt
+./writeResNumbers tests/inputs/008.convertPdbToGlycam_4YG0.pdb > 011.output_writeResidueNumbers.txt
 if [ -f 011.output.newNumbers.pdb ] && [ -f 011.output.original.pdb ]; then
     if ! cmp 011.output.newNumbers.pdb tests/correct_outputs/011.output.newNumbers.pdb > /dev/null 2>&1; then
         printf "Test FAILED! PDB file with new numbers is different\n"
