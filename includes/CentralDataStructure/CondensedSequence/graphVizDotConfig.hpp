e5efb6522035a6c67957ce039d430c19b26f0648
Preston <prestonj@uga.edu>
2023-07-30 23:15:36 -0400

    updated for more inteligent pathing
    graphviz config always finds the svgs and test 016 alters one of the correct outputs to what is expected on a test passing

--- includes/CentralDataStructure/CondensedSequence/graphVizDotConfig.hpp
+++ includes/CentralDataStructure/CondensedSequence/graphVizDotConfig.hpp
@@ -1,21 +1,40 @@
 #ifndef GMML_INCLUDES_CENTRALDATASTRUCTURE_CONDENSEDSEQUENCE_GRAPHVIZDOTCONFIG_HPP_
 #define GMML_INCLUDES_CENTRALDATASTRUCTURE_CONDENSEDSEQUENCE_GRAPHVIZDOTCONFIG_HPP_
 
+#include <filesystem>
+
+// unnamed namespace so we can grab the path we care about
+namespace
+{
+    std::string getSNFGPath()
+    {
+        std::filesystem::path snfgSVGCollectionPath = __FILE__;
+        snfgSVGCollectionPath                       = snfgSVGCollectionPath.parent_path().parent_path().parent_path();
+        snfgSVGCollectionPath                       /= "MolecularMetadata/Sugars/SNFG_Symbol_Images/";
+        return snfgSVGCollectionPath.string();
+    }
+
+    static std::string snfgSymbolsPath = getSNFGPath();
+} // namespace
+
 namespace cdsCondensedSequence
 {
+
+    // To help intelligently set the path of all our svg dudes
+
     struct GraphVizDotConfig
     {
-        GraphVizDotConfig(std::string svg_directory_path =
-                              "/programs/gems/gmml/includes/MolecularMetadata/Sugars/SNFG_Symbol_Images/")
-            : svg_directory_path_(svg_directory_path)
+        GraphVizDotConfig(std::string svg_directory_path = snfgSymbolsPath) : svg_directory_path_(svg_directory_path)
         {}
 
-        bool show_config_labels_        = true;
-        bool show_edge_labels_          = false;
-        bool show_position_labels_      = true;
-        int dpi_                        = 72;
-        std::string file_name_          = "oligosaccharide.dot";
-        std::string svg_directory_path_ = "/programs/gems/gmml/includes/MolecularMetadata/Sugars/SNFG_Symbol_Images/";
+        bool show_config_labels_   = true;
+        bool show_edge_labels_     = false;
+        bool show_position_labels_ = true;
+        int dpi_                   = 72;
+        std::string file_name_     = "oligosaccharide.dot";
+
+        std::string svg_directory_path_ = snfgSymbolsPath;
     };
+
 } // namespace cdsCondensedSequence
 #endif /* INCLUDES_INPUTSET_CONDENSEDSEQUENCE_GRAPHVIZDOTCONFIG_HPP_ */
