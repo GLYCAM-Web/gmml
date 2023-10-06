aa387f9f0845a11426fa1882b1408a913ea8bff8
Oliver <olgrant@uga.edu>
2023-10-06 10:33:08 +0200

    Bugfix for overrun detection.

--- src/CentralDataStructure/Readers/Pdb/pdbFunctions.cpp
+++ src/CentralDataStructure/Readers/Pdb/pdbFunctions.cpp
@@ -5,13 +5,15 @@
 int pdb::checkShiftFromSerialNumberOverrun(std::string line)
 {
     int shift = 0;
-    if (isdigit(line[12]) && line[22] != ' ')
+    if (isdigit(line[11]) && line[20] != ' ')
     {
-        shift = codeUtils::GetSizeOfIntInString(line.substr(12));
+        shift =
+            codeUtils::GetSizeOfIntInString(line.substr(12)); // The shift starts when this gets overrun, not 11. Right?
         std::stringstream ss;
-        ss << "Shift detected as position 12 is a digit: " << line[12] << " and this isn't blank: " << line[22] << "\n"
+        ss << "Shift of size " << shift << " detected as position 12 is a digit: " << line[11] << " and position 21 >>>"
+           << line[20] << "<<< isn't blank: " << line[20] << ":\n           v        v\n"
            << line << "\n";
-        gmml::log(__LINE__, __FILE__, gmml::ERR, ss.str());
+        gmml::log(__LINE__, __FILE__, gmml::WAR, ss.str());
     }
     return shift;
 }
