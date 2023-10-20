692b76e40d5c063feda0969aa39e360b89f4e99d
Oliver <olgrant@uga.edu>
2023-10-20 18:13:49 +0200

    Test changes to show new functionality.

--- tests/tests/026.test.editPDB.cpp
+++ tests/tests/026.test.editPDB.cpp
@@ -2,6 +2,10 @@
 #include "includes/CentralDataStructure/Readers/Pdb/pdbFile.hpp"
 #include "includes/CentralDataStructure/Readers/Pdb/pdbResidue.hpp"
 #include "includes/CentralDataStructure/Selections/residueSelections.hpp"
+#include "includes/CentralDataStructure/Selections/templatedSelections.hpp"
+#include "includes/CentralDataStructure/Writers/pdbWriter.hpp"
+
+#include <fstream> // std::ifstream
 
 int main(int argc, char* argv[])
 {
@@ -35,5 +39,26 @@ int main(int argc, char* argv[])
         ligandResidue->setName(firstLigandResidue->getName());
     }
     pdbFile.Write("./026.outputPdbFile.pdb");
+    // Separate thing showing this functionality to Yao.
+    // Don't want the selection updated every frame so do it outside the loop:#
+
+    cds::Ensemble selectionEnsemble;
+    for (auto& assembly : pdbFile.getAssemblies())
+    {
+        std::vector<cds::Residue*> myResidues = assembly->getResidues();
+        cds::Residue* queryResidue            = codeUtils::findElementWithNumber(
+            myResidues, 5); // somehow you specify this in inputs. e.g. A_405 chain A, residue 405.
+        double distanceFromQueryResidue             = 10.3; // inputs
+        std::vector<cds::Residue*> selectedResidues = cdsSelections::selectResiduesWithinDistanceN(
+            myResidues, queryResidue, distanceFromQueryResidue); // Need to write function.
+        cds::Assembly newAssembly(selectedResidues);             // Need to add constructor
+        selectionEnsemble.addAssembly(newAssembly);              // done
+    }
+    const std::string outName = "026.outputSelection.pdb";
+    std::ofstream outFileStream;
+    outFileStream.open(outName.c_str());
+    cds::writeEnsembleToPdb(outFileStream, selectionEnsemble.getAssemblies());
+    outFileStream.close();
+    pdbFile.Write("./026.outputPdbFileWithoutSelection.pdb");
     return 0;
 }
